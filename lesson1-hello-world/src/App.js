885e06622ebd98a420633e3dd2392113d207f216
braintop <braintop7@gmail.com>
2023-09-13 14:27:03 +0300

    add l9

--- lesson1-hello-world/src/App.js
+++ lesson1-hello-world/src/App.js
@@ -1,14 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./components/About";
-import Home from "./components/Home";
-import Movies from "./components/Movies";
-import NotFound from "./components/NotFound";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
-import Navbar2 from "./components/Navbar2";
-import Navbar from "./components/Navbar";
 
 function App() {
     //
@@ -18,18 +9,9 @@ function App() {
     };
     return (
         <div className="App">
-            {/* <Navbar /> */}
-            <BrowserRouter>
-                <Navbar2 />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/movies" element={<Movies />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/profile" element={<Profile />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </BrowserRouter>
+            <p>
+                Hello {user.firstname} {user.lastname}
+            </p>
         </div>
     );
 }
